442cf4c4070777cf0e4e81c55004358ccfa6f6fd
Syuhada Rantisi <suadarantisi@gmail.com>
2021-06-16 02:27:39 +0700

    Cmt ke-5. Add patch restartsig, restart dwm tanpa kill X server dan win lain + ganti shortcut

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -107,8 +107,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      try_quit,       {0} },
-	{ MODKEY|ShiftMask|ControlMask, XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_e,      try_quit,       {0} },
+	{ MODKEY|ShiftMask|ControlMask, XK_e,      quit,           {0} },
+	{ MODKEY,						XK_q,      quit,           {1} }, 
 };
 
 /* button definitions */
